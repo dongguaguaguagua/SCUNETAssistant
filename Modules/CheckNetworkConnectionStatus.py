f670a54b5620f46391f8f5661bb0d6ac7d51389e
nanguaguag <2022181640245@stu.scu.edu.cn>
2024-03-31 15:17:49 +0800

    检查网络是否连接更改为http://detectportal.firefox.com/success.txt

--- Modules/CheckNetworkConnectionStatus.py
+++ Modules/CheckNetworkConnectionStatus.py
@@ -2,9 +2,12 @@ from Modules.staticINFO import *
 
 
 def check_network_connection_status():
-    url = "https://www.baidu.com/"
+    url = "http://detectportal.firefox.com/success.txt"
     try:
-        if requests.get(url=url, headers=http_header, proxies=proxies).status_code == 200:
+        if requests.get(url=url, headers=http_header,
+                        proxies=proxies, timeout=5).text.strip() == 'success':
             return 0
-    except requests.exceptions.ConnectionError:
+    except requests.exceptions.ReadTimeout:
+        print('\033[1;91m' + '[请求超时]' + '\033[0m' + ', 即将重试')
         return -1
+    return -1
--- Modules/Exit.py
+++ Modules/Exit.py
@@ -1,8 +1,20 @@
 import sys
 import time
+from Modules.staticINFO import *
 
 
 def app_exit(delay_time):
     print('\033[1;93m' + '[自动退出]' + '\033[0m' + '程序即将自动退出')
     time.sleep(delay_time)
     sys.exit()
+
+
+def logout():
+    try:
+        url = main_url + "eportal/InterFace.do?method=logout"
+        logout_req = requests.post(url, data='')
+        if logout_req.status_code == 200:
+            print('\033[1;92m' + '[成功退出登录]' + '\033[0m')
+            return 0
+    except requests.exceptions.ConnectionError:
+        return -1
